345a6fb3df6ec03da591e41293d93a7b7dbe840d
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-04-07 10:33:08 -0400

    Updating childrensbooks.js

--- public/JS/childrensbooks.js
+++ public/JS/childrensbooks.js
@@ -2,10 +2,13 @@ var modal = document.getElementById("popup-modal");
 var closeButton = document.getElementById("close-button");
 var popupImage = document.getElementById("popup-image");
 var artistInfo = document.getElementById("artist-info");
+var prevButton = document.getElementById("prev-button");
+var nextButton = document.getElementById("next-button");
 
 var images = document.querySelectorAll("#childrensbooks img");
+var currentIndex = 0;
 
-images.forEach(function(image) {
+images.forEach(function(image, index) {
     image.addEventListener("click", function() {
         var imageSrc = this.getAttribute("src");
         var infoText = this.getAttribute("data-info");
@@ -13,6 +16,7 @@ images.forEach(function(image) {
         popupImage.src =imageSrc;
         artistInfo.textContent = infoText;
 
+        currentIndex = index; // Update the current index to the clicked image's index
         modal.style.display = "block";
     });
 });
@@ -25,4 +29,16 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+nextButton.onclick = function() {
+    currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image if at the end
+    var nextImage = images[currentIndex];
+    popupImage.src = nextImage.getAttribute("src");
+    artistInfo.textContent = nextImage.getAttribute("data-info");
+}
+prevButton.onclick = function() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length; // Loop back to the last image if at the beginning
+    var prevImage = images[currentIndex];
+    popupImage.src = prevImage.getAttribute("src");
+    artistInfo.textContent = prevImage.getAttribute("data-info");
 }
