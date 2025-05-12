525bbb12f062fb234ee490da2506ec946c4d5127
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-05-12 04:22:46 -0400

    Update script.js

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,22 +1,36 @@
-let slideIndex = 1;
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
 showSlides(slideIndex);
+
 
 function plusSlides(n) {
     showSlides((slideIndex += n));
-    }
+}
 
 function currentSlide(n) {
     showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-    const captionText = document.getElementById("caption");
+    let i; 
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    let captionText = document.getElementById("caption");
 
     if (n > slides.length) {slideIndex = 1;}
     if (n < 1) {slideIndex = slides.length;}
 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
